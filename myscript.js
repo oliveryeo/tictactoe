353f409d51ccda9f1db92329ccdd650a479b8435
oliveryjy <yjyoliver@hotmail.com>
2023-09-06 22:00:53 +0800

    created basic board template and pseudocode for js

---
+++ myscript.js
@@ -0,0 +1,12 @@
+// Factory Func for Players
+const Player = () => {
+  // add specific mark on the board → tag that space to the player → dont allow next player to click
+};
+
+// Module for gameBoard
+const gameBoard = (() => {
+  const board = [];
+
+  // if scoreTracker length is 9 → find the final results of the game
+  // find all possible winning combinations → check if any winning combination is satisfied by a player → that player wins
+})();
