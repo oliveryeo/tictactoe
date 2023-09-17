0ce8db49fadcf2145f94af800f8f3123a431754a
oliveryjy <yjyoliver@hotmail.com>
2023-09-17 18:51:21 +0800

    added first js logic draft for gameplay

--- myscript.js
+++ myscript.js
@@ -3,7 +3,7 @@ const Player = () => {
   // add specific mark on the board → tag that space to the player → dont allow next player to click
 };
 
-// Module for gameBoard
+// Module for Game Board
 const gameBoard = (() => {
   const rows = 3;
   const columns = 3;
@@ -12,11 +12,13 @@ const gameBoard = (() => {
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
-      board[i].push(cell());
+      board[i].push(Cell());
     }
   }
 
-  const selectGrid = (row, column, playerID) => {
+  const getBoard = () => board;
+
+  const playGrid = (row, column, playerID) => {
     const selectedGrid = board[row][column];
 
     // Create error handling where grid is already filled, hence player cannot select
@@ -24,21 +26,121 @@ const gameBoard = (() => {
     selectedGrid.addMark(playerID);
   }
 
-  return { selectGrid };
+  // Cell object for each grid in tic tac toe
+  function Cell() {
+    let value = 0;
+
+    const addMark = (playerID) => {
+      value = playerID;
+    };
+
+    const getValue = () => value;
+
+    return {
+      addMark,
+      getValue
+    };
+  }
+
+  return { playGrid, getBoard };
 })();
 
-// Cell object for each grid in tic tac toe
-const cell = (() => {
-  let value = 0;
+// Module for Game Controller
+const gameController = ((
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) => {
+  const players = [
+    {
+      name: playerOneName,
+      token: 1
+    },
+    {
+      name: playerTwoName,
+      token: 2
+    }
+  ];
 
-  const addMark = (playerID) => {
-    value = playerID;
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
 
-  const getValue = () => value;
+  const getActivePlayer = () => activePlayer;
+
+  const printPlayerTurn = () => {
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (row, column) => {
+    console.log(
+      `${getActivePlayer().name} has selected grid ${row}, ${column}...`
+    );
+    gameBoard.playGrid(row, column, getActivePlayer().token);
+    
+    /*  This is where we would check for a winner and handle that logic,
+    such as a win message. */
+
+    switchPlayerTurn();
+    printPlayerTurn();
+  };
 
   return {
-    addMark,
-    getValue
+    playRound,
+    getActivePlayer,
+    getBoard: gameBoard.getBoard
   };
+
 })();
+
+// Module for screenController
+const screenController = () => ({
+  const playerTurnDiv = document.querySelector(".turn");
+  const boardDiv = document.querySelector(".board");
+
+  const updateScreen = () => {
+    // clear the board
+    boardDiv.textContent = "";
+
+    // get the newest version of the board and player turn
+    const board = gameController.getBoard();
+    const activePlayer = gameController.getActivePlayer();
+
+    // Display player's turn
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+
+    // Render board squares
+    board.forEach((row, rowIndex) => {
+      row.forEach((cell, columnIndex) => {
+        // TODO -- tracked 16/09/2023
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        // Create a data attribute to identify the column
+        // This makes it easier to pass into our `playRound` function 
+        cellButton.dataset.row = rowIndex;
+        cellButton.dataset.column = columnIndex;
+        cellButton.textContent = cell.getValue();
+        boardDiv.appendChild(cellButton);
+      })
+    })
+  }
+  
+  // Add event listener for the board
+  function clickHandlerBoard(e) {
+    const selectedColumn = e.target.dataset.column;
+    // Make sure I've clicked a column and not the gaps in between
+    if (!selectedColumn) return;
+    
+    game.playRound(selectedColumn);
+    updateScreen();
+  }
+  boardDiv.addEventListener("click", clickHandlerBoard);
+
+    // Initial render
+    updateScreen();
+
+    // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+})();
+
+console.log(gameBoard.getBoard());
